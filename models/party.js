745914c49adab8795778e6f682fff44412de9c3a
Carl Reiner <creiner1@gmail.com>
2015-12-08 10:31:50 -0800

    user and party models

---
+++ models/party.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var PartySchema = mongoose.Schema({
+  date: Date,
+  users: [Schema.Types.ObjectId],
+  holiday:  String,
+  needs: [String]
+});
+
+// Let's craft how our JSON object should look!
+// http://mongoosejs.com/docs/api.html#document_Document-toObject
+// PartySchema.set('toJSON', {
+//    transform: function(doc, ret, options) {
+//        var returnJson = {
+//            id: ret._id,
+//            holiday: ret.holiday,
+//            needs: ret.needs,
+//        };
+//        return returnJson;
+//    }
+// });
+
+module.exports = mongoose.model('Party', PartySchema);
